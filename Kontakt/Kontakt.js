c0432dab58334b941282db455421d26d5dff44b1
Nix27 <nikolazecic2468@gmail.com>
2022-12-16 18:33:53 +0100

    Kontakt forma gotova

--- Kontakt/Kontakt.js
+++ Kontakt/Kontakt.js
@@ -1,4 +1,11 @@
 function ShowForm(){
-    const forma = document.getElementById("kontakt");
+    var forma = document.getElementById("bgKontakt");
     forma.style.display = "block";
+    document.body.style.overflow = "hidden";
+}
+
+function HideForm(){
+    var forma = document.getElementById("bgKontakt");
+    forma.style.display = "none";
+    document.body.style.overflow = "visible";
 }
