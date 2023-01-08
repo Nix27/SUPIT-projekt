821d8f0b3a07b622d19b6aeceed3c1bac592ccb0
Nix27 <nikolazecic2468@gmail.com>
2023-01-09 00:05:41 +0100

    Krenuo s tablicom na nastavnom planu

--- Nastavni plan/NastavniPlan.js
+++ Nastavni plan/NastavniPlan.js
@@ -13,12 +13,17 @@ $(() => {
     $('#odabirKolegija').autocomplete({
         source: async (request, response) => {
             ucitajKolegije().then(data => {
-                const filteredData = data.filter((item) => item.kolegij.startsWith(request.term));
+                const filteredData = data.filter((item) => item.kolegij.includes(request.term));
                 response(filteredData.map((item) => item.kolegij));
             });
         },
-        select: () => {
-            console.log($('table'));
+        select: async (e, ui) => {
+            $('table').append('<tr><th>Kolegij</th><th>ECTS</th><th>Sati</th><th>Predavanja</th><th>Vježbe</th><th>Tip</th></tr>');
+            ucitajKolegije().then(data => {
+                const filteredData = data.filter((item) => item.kolegij === ui.item.label);
+                const kolegij = filteredData.map((item) => item);
+                $('table').append(`<tr><td>${kolegij[0].kolegij}</td><td>${kolegij[0].ects}</td><td>${kolegij[0].sati}</td><td>${kolegij[0].predavanja}</td><td>${kolegij[0].vjezbe}</td><td>${kolegij[0].tip}</td></tr>`);
+            });
         }
     });
 });
